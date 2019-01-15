338f079bf44ca4bb8a44bb0622f9a06851d42432
Rosilde Tatiana Irene <tatia@Air-di-Rosilde.station>
2019-01-15 20:16:36 +0100

    added reusable component and included Header

---
+++ src/components/common/index.js
@@ -0,0 +1,7 @@
+export * from './Button';
+export * from './Card';
+export * from './CardSection';
+export * from './Header';
+
+//file used as buffer to import all common components
+//NB in this case the components will not be exported as default, but as objects (es. export {Button:Button})
