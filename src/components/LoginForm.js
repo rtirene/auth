5f05de06330109b1e6ed8c0e9732e244ddfc1ab0
Rosilde Tatiana Irene <tatia@Air-di-Rosilde.station>
2019-01-19 19:08:12 +0100

    handling auth and errors

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,23 +1,91 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
-import { Button, Card, CardSection } from './common';
+import { Text } from 'react-native'
+import { Button, Card, CardSection, Input, Spinner } from './common';
+import firebase from 'firebase';
 
 class LoginForm extends Component{
+  state = { email : '', password : '', error: '', loading: false};
+  onButtonPress() {
+    const { email, password } = this.state;
+    this.setState({ error: '', loading: true });
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      .then(this.onLoginSuccess.bind(this))
+      .catch(() => {
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+          .then(this.onLoginSuccess.bind(this))
+          .catch(this.onLoginFail.bind(this));
+      });
+  }
+
+  onLoginFail() {
+   this.setState({ error: 'Authentication Failed', loading: false });
+ }
+
+ onLoginSuccess() {
+   this.setState({
+     email: '',
+     password: '',
+     loading: false,
+     error: ''
+   });
+ }
+
+ renderButton(){
+   if (this.state.loading){
+     return <Spinner size = 'small' />;
+   }
+   return(
+     <Button onPress={this.onButtonPress.bind(this)}>
+         Log in!
+     </Button>
+   );
+ }
+
   render(){
     return(
       <Card>
-        <CardSection />
-
-        <CardSection />
-        
         <CardSection>
-          <Button>
-              Log in!
-          </Button>
+          <Input
+          secureTextEntry = {false}
+          placeholder= 'user@gmail.com'
+          label = 'Email'
+          value = {this.state.email} //initialize text as the current state
+          onChangeText={email=> this.setState({ email })} //whenever the user modifies text the state is set again
+          //aka the component rerenders
+          //these props are passed to the Input component
+          />
+        </CardSection>
+
+        <CardSection>
+          <Input
+            secureTextEntry = {true}
+            placeholder = 'password'
+            label = 'Password'
+            value = {this.state.password}
+            onChangeText={password=> this.setState({ password })}
+          />
+        </CardSection>
+
+        <Text style={styles.errorTextStyle}>
+          {this.state.error}
+        </Text>
+
+        <CardSection>
+          {this.renderButton()}
         </CardSection>
       </Card>
     );
   }
 }
+
+const styles  = {
+    errorTextStyle: {
+      fontSize: 20,
+      alignSelf: 'center',
+      color: 'red'
+    }
+
+}
+
 
 export default LoginForm;
--- src/components/common/index.js
+++ src/components/common/index.js
@@ -2,6 +2,8 @@ export * from './Button';
 export * from './Card';
 export * from './CardSection';
 export * from './Header';
+export * from './Input';
+export * from './Spinner';
 
 //file used as buffer to import all common components
 //NB in this case the components will not be exported as default, but as objects (es. export {Button:Button})
